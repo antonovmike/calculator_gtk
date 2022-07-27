c42573861ca5763cf8a6fdbbef572b2bab32a0c0
Mihail Antonov <antonovma@outlook.com>
2022-07-27 22:02:54 +0400

    functions.rs

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,7 @@
-// use std::io;
-// use gtk::prelude::*;
-
-// fn main() {
-//     let mut input = String::new();
-//     io::stdin().read_line(&mut input).expect("Failed to read line");
-
-//     let vector: Vec<char> = input.chars().collect();
-//     let first_number = (vector[0].to_string()).parse::<f32>().unwrap();
-//     let operator = vector[1];
-//     let second_number = (vector[2].to_string()).parse::<f32>().unwrap();
-
-//     let result = operate(operator, first_number, second_number);
-
-//     println!(
-//         "{:?}",
-//         output(first_number, operator, second_number, result)
-//     );
-// }
-
-// fn operate(operator: char, first_number: f32, second_number: f32) -> String {
-//     match operator {
-//         '+' => (first_number + second_number).to_string(),
-//         '-' => (first_number - second_number).to_string(),
-//         '/' => (first_number / second_number).to_string(),
-//         '*' | 'x' | 'X' => (first_number * second_number).to_string(),
-//                _ => ("Invalid operator used.").to_string()
-//     }
-// }
-
-// fn output(first_number: f32, operator: char, second_number: f32, result: String) -> String {
-//     format!(
-//         "{} {} {} = {}",
-//         first_number, operator, second_number, result
-//     )
-// }
-
 use gtk::prelude::*;
 
 mod gui;
+mod functions;
 
 fn main() {
     let application = gtk::Application::new(
