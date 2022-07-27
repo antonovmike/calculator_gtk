616af17aee0d1ac747cddd7ee1c85dcd7de51a20
Mihail Antonov <antonovma@outlook.com>
2022-07-27 22:06:33 +0400

    functions.rs

--- src/gui.rs
+++ src/gui.rs
@@ -4,6 +4,8 @@ use std::rc::Rc;
 use gtk::prelude::*;
 use glib_macros::clone;
 
+use crate::functions::{operation, the_result, set_value};
+
 pub const ADD: char = '+';
 pub const SUBTRACT: char = '-';
 pub const MULTIPLY: char = '*';
@@ -292,75 +294,4 @@ pub fn build_ui(application: &gtk::Application) {
     grid.attach(&clear_button, 3, 0, 1, 1);
 
     window.show_all();
-}
-
-pub fn set_value(num_counter: i32, dot_counter: i32, value_1: &Rc<Cell<f64>>, value_2: &Rc<Cell<f64>>, num: f64) {
-    if dot_counter == 0 {
-        println!("-> dot_counter == 0");
-        if num_counter == 0 {
-            value_1.set(value_1.get() * 10.0 + num);
-        }
-        if num_counter == 1 {
-            value_2.set(value_2.get() * 10.0 + num);
-        }
-    }
-    if dot_counter == 1 {
-        println!("-> dot_counter == 1");
-        if num_counter == 0 {
-            value_1.set(value_1.get() + num / 10.0);
-        }
-        if num_counter == 1 {
-            value_2.set(value_2.get() * 10.0 + num);
-        }
-    }
-    if dot_counter == 2 {
-        println!("-> dot_counter == 2");
-        if num_counter == 0 {
-            value_1.set(value_1.get() * 10.0 + num);
-        }
-        if num_counter == 1 {
-            value_2.set(value_2.get() + num / 10.0);
-        }
-    }
-}
-
-pub fn operation(previous_operation: char, value_1: &Rc<Cell<f64>>, value_2: &Rc<Cell<f64>>) {
-    match previous_operation {
-        ADD =>      { value_1.set(value_1.get() + value_2.get()); },
-        SUBTRACT => { value_1.set(value_1.get() - value_2.get()); },
-        MULTIPLY => { value_1.set(value_1.get() * value_2.get()); },
-        DIVIDE =>   { value_1.set(value_1.get() / value_2.get()); },
-        _=> ()
-    }
-}
-
-fn the_result(current_operation: char, value_1: &Rc<Cell<f64>>, value_2: &Rc<Cell<f64>>) -> std::string::String {
-    let mut result = String::from(" = ");
-    // Add operation symbol to variable
-    let operation_symbol = match current_operation {
-        ADD =>      "+",
-        SUBTRACT => "-",
-        MULTIPLY => "\u{00D7}",
-        DIVIDE =>   "\u{00F7}",
-        _=>         "Error"
-    };
-
-    let operation_string = format!("{}{}{}", value_1.get(), operation_symbol, value_2.get());
-
-    match current_operation {
-        ADD =>      { value_1.set(value_1.get() + value_2.get()); },
-        SUBTRACT => { value_1.set(value_1.get() - value_2.get()); },
-        MULTIPLY => { value_1.set(value_1.get() * value_2.get()); },
-        DIVIDE =>   { value_1.set(value_1.get() / value_2.get()); },
-        _=> ()
-    }
-    if current_operation == DIVIDE && value_2.get() == 0.0 {
-        result =  String::from("Error: divide by 0");
-    }
-    else {
-        result.push_str( &value_1.get().to_string() );
-        result = format!("{}{} ", operation_string, result)
-    }
-
-    result
 }
