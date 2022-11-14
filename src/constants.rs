af2d873f49a6f67c634a545e2ce4ad03581b5cb3
antonovmike <antonvma@outlook.com>
2022-11-14 17:40:22 +0500

    removed extra code

--- src/constants.rs
+++ src/constants.rs
@@ -2,5 +2,5 @@ pub const ADD: char = '+';
 pub const SUBTRACT: char = '-';
 pub const MULTIPLY: char = '*';
 pub const DIVIDE: char = '/';
-pub const EQUALS: char = '=';
+// pub const EQUALS: char = '=';
 pub const NONE: char = '0';
