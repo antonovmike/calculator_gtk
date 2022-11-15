0a9b9cfdf1293e324c03b5c967021e452ac3cded
antonovmike <antonvma@outlook.com>
2022-11-15 10:26:48 +0500

    extra zeroes remover

--- src/functions.rs
+++ src/functions.rs
@@ -8,8 +8,8 @@ pub fn entry_parser(entry_data: String, equals: bool) -> String {
         match operand {
             '+' => (v[0].parse::<f64>().unwrap() + v[2].parse::<f64>().unwrap()).to_string(),
             '-' => (v[0].parse::<f64>().unwrap() - v[2].parse::<f64>().unwrap()).to_string(),
-            '×' => format!("{:.6}", v[0].parse::<f64>().unwrap() * v[2].parse::<f64>().unwrap()),
-            '÷' => format!("{:.6}", v[0].parse::<f64>().unwrap() / v[2].parse::<f64>().unwrap()),
+            '×' => format!("{}", extra_zeroes_remover(v[0].parse::<f64>().unwrap() * v[2].parse::<f64>().unwrap())),
+            '÷' => format!("{}", extra_zeroes_remover(v[0].parse::<f64>().unwrap() / v[2].parse::<f64>().unwrap())),
             _ => "0".to_string(),
         }
     } else { "0".to_string() };
@@ -18,6 +18,20 @@ pub fn entry_parser(entry_data: String, equals: bool) -> String {
     println!("result = {}", result);
 
     result
+}
+
+fn extra_zeroes_remover(f: f64) -> String {
+    let string0 = format!("{}", f);
+    let mut vec: Vec<char> = string0.chars().collect();
+    for _i in 0..vec.len() {
+        let last_element = vec[vec.len() - 1];
+        if last_element == '0' {
+            vec.remove(vec.len() - 1);
+        }
+    }
+    println!("{:?}", vec);
+    let string1: String = vec.iter().collect::<String>();
+    string1
 }
 
 // #[test]
