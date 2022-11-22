fbe05f277dd740694e9f88b7c327d77d29445d9a
antonovmike <antonvma@outlook.com>
2022-11-22 17:06:59 +0500

    wrong input check, fix "a1" input

--- src/functions.rs
+++ src/functions.rs
@@ -1,7 +1,15 @@
 // #[allow(dead_code)]
 
 pub fn entry_parser(entry_data: String) -> String {
-    if entry_data.len() != 0 {
+    // Check if Entry is sempty string 
+    // Check if only one number 
+    // Check if Entry contains only letters
+    // Add char + digit "a1" checker
+    // Check if first number and operand with no second number
+    if entry_data.len() != 0 
+    && entry_data.chars().all(|x| x.is_alphanumeric()) == false 
+    // && entry_data.chars().all(|x| x.is_ascii_alphabetic())
+    && entry_data.chars().last().unwrap().is_numeric() {
         let v: Vec<&str> = entry_data.split(' ').collect();
         let operand = v[1].parse::<char>().unwrap();
 
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -5,17 +5,36 @@ fn empty() {
     let entry_data = "".to_string();
     assert_eq!("0", functions::entry_parser(entry_data))
 }
+
+#[test]
+fn wrong_input() {
+    let entry_data = "-".to_string();
+    assert_eq!("0", functions::entry_parser(entry_data));
+    let entry_data = "1 -".to_string();
+    assert_eq!("0", functions::entry_parser(entry_data));
+    let entry_data = "1".to_string();
+    assert_eq!("0", functions::entry_parser(entry_data));
+    let entry_data = ".".to_string();
+    assert_eq!("0", functions::entry_parser(entry_data));
+    let entry_data = "stop_war".to_string();
+    assert_eq!("0", functions::entry_parser(entry_data));
+    // let entry_data = "abcd1234".to_string();
+    // assert_eq!("0", functions::entry_parser(entry_data))
+}
+
 #[test]
 fn add() {
     let entry_data = "1.022 + 3.009".to_string();
     assert_eq!("4.031", &functions::entry_parser(entry_data))
 }
+
 #[test]
 fn sub() {
     let entry_data = "1.022 - 3.009".to_string();
     assert_eq!("-1.9869999999999999", &functions::entry_parser(entry_data))
     // Have to be 1.987
 }
+
 #[test]
 fn mul() {
     let entry_data = "1.022 × 3.009".to_string();
